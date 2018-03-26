99972997d41ad7e1dab079bf837e76b6d92f51d0
Eric <eric.mercado60@hotmail.com>
2018-03-25 22:12:33 -0500

    Final

--- my-app/src/components/Header/Header.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Header = props => 
-
-	<header className="App-header">
-	  <h1 className="App-title">Eric Mercado's Clicky Game, BOI</h1>
-	</header>
-export default Header;
